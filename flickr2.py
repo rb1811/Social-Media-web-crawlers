4141e07a5a421c6e350245be2b8f5f3c66fd1b40
rb1811 <pracherl@asu.edu>
2017-03-29 17:05:20 -0700

    flickr done

--- flickr2.py
+++ flickr2.py
@@ -1,46 +1,86 @@
 import flickrapi
 import webbrowser
 import json
+import time
 flickrapi_call = 0
 key_count  = 0
 
-# api_key1 = '2b3b4d7eaf7dd7527e42eebd7b66e5e8'
-# api_secret1 = '678c6ca91b73eb23'
+api_key100 = '2b3b4d7eaf7dd7527e42eebd7b66e5e8'
+api_secret100 = '678c6ca91b73eb23'
 
-api_key1 = 'a91745e3de764fb547d1c0547f8167cd'
-api_secret1 = '16147f1695cae970'
+api_key1 = '0930efb0108ba641e1a9227e3a288711'
+api_secret1 = '9c7fa568e1a1c156'
 
-api_key2 = 'b384fb49fb89bb1ca9069f31d2ab4429'
-api_secret2 = '8bf159ca0c475690'
+api_key2 = '27ccfa8191c137ba7cf3caac08df1867'
+api_secret2 = '16f16cb7e9282c06'
 
-api_key3 = 'bdaed96fbcb617d03c023d135f6dd6f7'
-api_secret3 = '44662dbf608a86ad'
+api_key3 = '979f430d1a94852bb4b30bcbd3aa4267'
+api_secret3 = '00f656b082ea420b'
 
-api_key4 = '7b4bfd3040ad13d2f857c15f22752112'
-api_secret4 = '3cc7a38746b064a6'
+api_key4 = 'b81f4cb4a57847a472b3dab2c19e64ed'
+api_secret4 = '7a418b250a7dedb0'
 
-api_key5 = '14e70d57122324b7683e3caefe04763c'
-api_secret5 = '3a1cfedd7f06448e'
+api_key5 = 'ecdad53eb5ce7696e26794910df42442'
+api_secret5 = 'd8ae6a5ca3da64ee'
 
-api_key6 = 'f09113731ee9762dd193d1b069324b81'
-api_secret6 = '36bdfe5fe7255a85'
+api_key6 = 'd2ebd8db92be92d8bfa0927c954606cd'
+api_secret6 = 'a9122939c82ad2ef'
 
-api_key7 = '48c92fa738627e95d3528bddeaf672c9'
-api_secret7 = '5c9bffaa8638c5c9'
+api_key7 = '43133ed8c1048410a0fe5e466fef7b49'
+api_secret7 = '7dfe2e8548905b88'
 
-api_key8 = '6974d8901d773e682e183bdd99909aad'
-api_secret8 = '08baf3c9200381e0'
+api_key8 = '2a6fa38766181be9dc43dcb7199c8101'
+api_secret8 = '6903c641681f518c'
+
+api_key9 = 'b9e0ab0e506abf9d201c7dd72cbaaeb0'
+api_secret9 = '1abf5f6e360f5942'
+
+api_key10 = 'b993247a26d8d0213aee608924f34ce0'
+api_secret10 = 'ca2f2b4f477649ae'
+
+api_key11 = '7e078136f49c696ed84b0c7e58fb43d4'
+api_secret11 = '9be4f25b1b887e51'
+
+api_key12 = '4b4481899eb31b97beccdc6e4f719bf3'
+api_secret12 = '3ef5cc139ecf794f'
+
+api_key13 = 'cc36816ff9611e73a0ce5a87440f9d5c'
+api_secret13 = 'c2fbf0fc7706e660'
+
+api_key14 = '977b181d7cf823072121ce9b7edcb3f4'
+api_secret14 = 'f3d73f4052b09afe'
+
+api_key15 = 'feabd79fc61e28ace34d1f9c7d2bc7cb'
+api_secret15 = 'd3e87445e822ec53'
+
+api_key16 = 'bf4116363fd8726409207f0e808cc01b'
+api_secret16 = '3e2fe6ca53b5aa53'
+
+api_key17 = '47c279082e9b138d57746d91e6a28c6f'
+api_secret17 = '32154a07aab5e678'
+
+
 
 key_list=[
-
+[api_key100,  api_secret100],
+[api_key1,  api_secret1],
+[api_key2,  api_secret2],
 [api_key3,  api_secret3],
 [api_key4,  api_secret4],
 [api_key5,  api_secret5],
 [api_key6,  api_secret6],
 [api_key7,  api_secret7],
 [api_key8,  api_secret8],
-[api_key2,  api_secret2],
-[api_key1,  api_secret1],
+[api_key9,  api_secret9],
+[api_key10,  api_secret10],
+[api_key11,  api_secret11],
+[api_key12,  api_secret12],
+[api_key13,  api_secret13],
+[api_key14,  api_secret14],
+[api_key15,  api_secret15],
+[api_key16,  api_secret16],
+[api_key17,  api_secret17],
+
 ]
 
 
@@ -74,47 +114,55 @@ flickrdata = []
 f= open('flickrusernames.txt','r')
 data =f.read()
 data =  data.split('\n')
-
-
+unknown= []
+# testcount=100
 for link in data:
-    if not 'flickr' in link:
-        continue
-    else:
-        link = link[:-1]
-        print flickrapi_call, "::::" ,link
-        flickrapi_call+=1
-        if flickrapi_call>1798:
-            flickrapi_call = 0
-            key_count+=1
-            if key_count == 8:
-                key_count = 0
-                print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-                print "The 30 mins delay has started to refresh the keys"
-                print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
-                time.sleep(1800)
+    # testcount -=1
+    # if testcount == 0:
+    #     break
+    # # if not 'flickr' in link:
+    #     continue
+    # else:
+    link = link[:-1]
+    print flickrapi_call, "::::" ,link
+    flickrapi_call+=1
+    if flickrapi_call>1798:
+        flickrapi_call = 0
+        key_count+=1
+        if key_count == 18:
+            key_count = 0
+            print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+            print "The 15 mins delay has started to refresh the keys"
+            print "$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$"
+            time.sleep(900)
 
-        username =  link[link.rfind('/')+1:]
+    username =  link[link.rfind('/')+1:]
+    username = ''.join(e for e in username if e.isalnum())
+    # print username
+    try:
         flickr = flickrapi.FlickrAPI(key_list[key_count][0], key_list[key_count][1],format='parsed-json')
+        resp = flickr.people.findByUsername(username = username)
+        if resp['stat'] == "ok":    
+            userid = resp['user']['nsid']
+            resp = flickr.profile.getProfile(user_id=userid)
+            
 
-        resp= flickr.people.findByUsername(username = username)
-        userid = resp['user']['nsid']
-        resp = flickr.profile.getProfile(user_id=userid)
-        # User not found
-        if resp['stat'] == "ok":
-            # data ={}
-            # data = {
-            #  "profile_description" :  resp['profile']['profile_description'],
-            #      "city"  :  resp['profile']['city'],
-            # "first_name" :  resp['profile']['first_name'],
-            #  "last_name" :  resp['profile']['last_name'],
-            #    "hometown":  resp['profile']['hometown'],
-            #    "twitter" :  resp['profile']['twitter'],
-            #  "country" :  resp['profile']['country'],
-            # "occupation" :  resp['profile']['occupation']
-            # }
             print "This is the key being used", key_count
             flickrdata.append({  resp['profile']['nsid'].encode('ascii','ignore'): resp['profile']})
+
+    except Exception:
+        unknown.append(link) # Its an array to store all those urls who are not found
+    # else:
+        
+    
 g= open('flickr2.json','w')
-g.write(str(flickrdata))
+for data in flickrdata:
+    g.write(str(data)+'\n')
+
 g.close()
 f.close()
+g= open('unknownflicker.txt','w')
+for link in unknown:
+    g.write(str(link)+'\n')    
+g.close()
+print unknown
--- flickrtest.py
+++ flickrtest.py
@@ -1,6 +1,7 @@
 import flickrapi
 import webbrowser
 import json
+import sys
 api_key = '2b3b4d7eaf7dd7527e42eebd7b66e5e8'
 api_secret = '678c6ca91b73eb23'
 # print('Step 1: authenticate')
@@ -31,33 +32,39 @@ api_secret = '678c6ca91b73eb23'
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret,format='parsed-json')
 
+print "qweww"
+try:
+    resp= flickr.people.findByUsername(username = 'ajoci')
+except flickrapi.exceptions.FlickrError:
+    print('could not get info')
+else:    
+    print "asdasd"
+    print resp
+    userid = resp['user']['nsid']
+    print userid
+    resp = flickr.profile.getProfile(user_id=userid)
+    resp = dict(resp)
+    # resp = json.loads(resp.decode('utf-8')) 45301915@N00
+    print resp.keys()
+    print resp['stat']
+    print resp['profile']
+    alldata = []
+    # data = {
+    #            "profile_description" :  resp['profile']['profile_description'],
+    #                 "city"  :  resp['profile']['city'].encode('ascii','ignore'),
+    #         "first_name" :  resp['profile']['first_name'].encode('ascii','ignore'),
+    #          "last_name" :  resp['profile']['last_name'].encode('ascii','ignore'),
+    #             # "nsid"  :  resp['profile']['nsid'].encode('ascii','ignore'),
+    #            "hometown":  resp['profile']['hometown'].encode('ascii','ignore'),
+    #            "twitter" :  resp['profile']['twitter'].encode('ascii','ignore'),
+    #          "country" :  resp['profile']['country'].encode('ascii','ignore'),
+    #         "occupation" :  resp['profile']['occupation'].encode('ascii','ignore')
+    #         }
 
-resp= flickr.people.findByUsername(username = 'emmerogers')
-userid = resp['user']['nsid']
-print userid
-resp = flickr.profile.getProfile(user_id=userid)
-resp = dict(resp)
-# resp = json.loads(resp.decode('utf-8')) 45301915@N00
-print resp.keys()
-print resp['stat']
-print resp['profile']
-alldata = []
-# data = {
-#            "profile_description" :  resp['profile']['profile_description'],
-#                 "city"  :  resp['profile']['city'].encode('ascii','ignore'),
-#         "first_name" :  resp['profile']['first_name'].encode('ascii','ignore'),
-#          "last_name" :  resp['profile']['last_name'].encode('ascii','ignore'),
-#             # "nsid"  :  resp['profile']['nsid'].encode('ascii','ignore'),
-#            "hometown":  resp['profile']['hometown'].encode('ascii','ignore'),
-#            "twitter" :  resp['profile']['twitter'].encode('ascii','ignore'),
-#          "country" :  resp['profile']['country'].encode('ascii','ignore'),
-#         "occupation" :  resp['profile']['occupation'].encode('ascii','ignore')
-#         }
 
+    # print data
 
-# print data
-
-alldata.append({resp['profile']['nsid'].encode('ascii','ignore'): resp['profile']})
-g= open('flickrtest.json','w')
-g.write(str(alldata))
-g.close()
+    alldata.append({resp['profile']['nsid'].encode('ascii','ignore'): resp['profile']})
+    g= open('flickrtest.json','w')
+    g.write(str(alldata))
+    g.close()
